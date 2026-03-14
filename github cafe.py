f249d085a7e2242c418fc525aa4ab70e0aa9e0c7
rebeccaoyewuwo <ro294@kent.ac.uk>
2026-03-14 22:59:40 +0000

    class menu item

--- github cafe.py
+++ github cafe.py
@@ -5,3 +5,14 @@ import re
 def clean_input(text):
     return re.sub(r"\s+", " ", text).strip()
 #removing extra spaces, tabs and double spaces
+
+class MenuItem:
+    def __init__ (self, name, price, item_type, ingredients, steps, sizes):
+        self.name = name
+        self.price = price
+        self.type = item_type
+        self.ingredients = ingredients 
+        self.steps = steps
+        self.sizes = sizes
+
+classInventory
