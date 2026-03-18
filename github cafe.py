074114284092ef01801fb0d906ec549a743d6250
rebeccaoyewuwo <ro294@kent.ac.uk>
2026-03-18 19:46:02 +0000

    inventory stock

--- github cafe.py
+++ github cafe.py
@@ -27,3 +27,25 @@ class Inventory:
     def update_stock(self, item, qty=1):
         for ingredient, amount in item.ingredients.items():
             self.stock[ingredient] -= amount * qty    
+
+# inventory stock for the cafe
+inventory_stock = {
+    "Coffee Beans": 100,
+    "Oat Milk": 100,
+    "Cups": 100,
+    "Paper Food Bag": 100,
+    "Bread": 75,
+    "Chocolate Syrup": 50,
+    "Mayo": 50,
+    "Matcha Powder": 45,
+    "Chai Powder": 45,
+    "Cinnamon Powder": 40,
+    "Tomato": 40,
+    "Lettuce": 40,
+    "Egg": 40,
+    "Ham": 20,
+    "Cheese": 20,
+    "Vegan Ham": 20,
+    "Tuna": 20,
+    "Avocado": 20,
+}
